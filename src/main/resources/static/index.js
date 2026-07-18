37a5da8d2f2516112f260841d6f2ac4e9dcbf350
Hrushikesh <25071a7228@vnrvjiet.in>
2026-07-18 12:45:22 +0530

    added randomuser

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -2,3 +2,51 @@
 //     "Hello from js!"
 // )
 console.log("Hello from js!")
+// data ={
+//     name: "Bob",
+//     gender: "male"
+// }
+var users= [
+    {
+        "name": "Bob",
+        "gender": "male",
+        "image":"john.png"
+    },
+        {
+        "name": "Jane doe",
+        "gender": "female",
+        "image":"jane.png"
+    }
+]
+var id=0;
+function toggleuser() {
+    id =(id+1)%2
+    var userImage = document.getElementById("userImage");
+    userImage.src = users[id].image;
+    var userName = document.getElementById("username");
+    userName.innerHTML = users[id].name;
+    var userGender = document.getElementById("usergender");
+    userGender.innerHTML = users[id].gender;
+}
+function randomuser() {
+    fetch("https://randomuser.me/api/")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            var usersData = data.results[0];
+
+            var userImage = document.getElementById("userImage");
+            userImage.src = usersData.picture.large;
+
+            var userName = document.getElementById("username");
+            userName.innerHTML =
+                usersData.name.first + " " + usersData.name.last;
+
+            var userGender = document.getElementById("usergender");
+            userGender.innerHTML = usersData.gender;
+        })
+        .catch(function(error) {
+            console.log("Error fetching user data:", error);
+        });
+}
